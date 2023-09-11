6b3a2c2c1f690fe05457e03388cac45d42368908
jhonathan55 <jfgutierrezsaico@gmail.com>
2023-09-11 14:19:05 -0300

    redireccion ok

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,39 @@
 import { Component } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  componente:Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'airplane-outline',
+      name: 'Alert',
+      redirecTo: '/alert'
+    },
+    {
+      icon: 'beaker',
+      name: 'Avatar',
+      redirecTo: '/avatar'
+    },
+  ];
+  constructor() { }
 
-  constructor() {}
-
-  aa(){
+  aa() {
     console.log("aa");
-    
+
   }
 
 }
